bf1adb88b2a8a5718cd965c217ac747dbb28809b
Alexey Zauzin <lehadnk@gmail.com>
2017-02-15 05:09:17 +0300

    Minified version of the files

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@
  * Created by lehadnk on 10/02/2017.
  */
 var gulp = require('gulp');
-var minify = require('gulp-minify');
+var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
 gulp.task('build', function() {
-    return gulp.src(['ImageUploadForm.js', 'ImageUploadForm.css'])
+    gulp.src(['ImageUploadForm.css'])
         .pipe(minify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('build/'));
+
+    gulp.src(['ImageUploadForm.js'])
+        .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('build/'));
 });
