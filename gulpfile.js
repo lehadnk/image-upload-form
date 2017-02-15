83c78b47a81db65b6c0878a875e21944580f7de3
Alexey Zauzin <lehadnk@gmail.com>
2017-02-15 07:32:36 +0300

    Git project page

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,18 @@ var gulp = require('gulp');
 var minify = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var copy = require('gulp-copy');
 
 gulp.task('build', function() {
     gulp.src(['ImageUploadForm.css'])
         .pipe(minify())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(copy('docs/assets/'));
 
     gulp.src(['ImageUploadForm.js'])
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('build/'))
+        .pipe(copy('docs/assets/'));
 });
